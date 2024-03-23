c0dca4928183c3ea8bf245c87c357bb3a7297f12
Sriram <sriram.vilvanathan@rbc.com>
2024-03-23 10:18:38 +0530

    basic app to fetch data from an api

--- Learn/app1/src/App.js
+++ Learn/app1/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [advice, setAdvise] = useState("");
+
+  async function getAdvise() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvise(data.slip.advice);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <button onClick={getAdvise}>get Advise</button>
+      <p>{advice}</p>
+    </>
   );
 }
 
